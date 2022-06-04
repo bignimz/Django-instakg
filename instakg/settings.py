676d1795a9ddce160b032763badff17ddd3a5018
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-04 17:15:03 +0300

    modified the settings.py file

--- instakg/settings.py
+++ instakg/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # Custom Apps
     'post',
+    # 'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -132,10 +133,10 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_REDIRECT_URL = 'elements:home'
+LOGIN_REDIRECT_URL = ''
 
-LOGOUT_REDIRECT_URL = 'userauths:login'
+LOGOUT_REDIRECT_URL = ''
 
-LOGIN_URL = 'userauths:login'
+LOGIN_URL = ''
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
