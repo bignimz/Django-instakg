b3ab3fd02940b67d734538f82708e64202553ee7
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-04 16:25:17 +0300

    added url paths for static, media, login, logout and crispy templates in the settings.py

--- instakg/settings.py
+++ instakg/settings.py
@@ -121,3 +121,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = 'elements:home'
+
+LOGOUT_REDIRECT_URL = 'userauths:login'
+
+LOGIN_URL = 'userauths:login'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
