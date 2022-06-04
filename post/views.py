910f2458a1d0b4ce8a316012bbf5db8e946aa4b5
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-04 17:11:40 +0300

    created index view

--- post/views.py
+++ post/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    # Getting the instance of the current logged in user
+    current_user = request.user
+    return render(request, 'index.html')
