4e7279db1515d4e5ddf745595ef4edd3d4e79509
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-05 23:03:05 +0300

    added the new post view path to the post urls.py

--- post/urls.py
+++ post/urls.py
@@ -3,4 +3,6 @@ from post import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new_post/', views.new_post, name='new_post'),
+    path('<uuid:post_id>/', views.post_detail, name='post_detail'),
 ]
