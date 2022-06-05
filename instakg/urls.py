f4a6b225efe9dd872a2a25670652a599fe80fc2e
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-05 23:01:16 +0300

    added the new post path to the url patterns of the project

--- instakg/urls.py
+++ instakg/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('post.urls'))
+    path('post/', include('post.urls')),
+
 ]
 
 if settings.DEBUG:
