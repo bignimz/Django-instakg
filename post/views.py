288e6b23b36f564ce92762e54d4a475839b971a3
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-05 23:02:18 +0300

    created a new post view

--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from post.models import Tag, Post, Follow, Stream
 from django.contrib.auth.decorators import login_required
+from post.forms import PostForm
 
 def index(request):
     # Getting the instance of the current logged in user
@@ -13,3 +14,37 @@ def index(request):
     context = {'post_items': post_items}
 
     return render(request, 'index.html', context)
+
+
+def new_post(request):
+    user = request.user.id
+    tags_objs = []
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            picture = form.cleaned_data.get('picture')
+            caption = form.cleaned_data.get('caption')
+            tag_form = form.cleaned_data.get('tag')
+
+            tags_list = list(tag_form.split(','))
+            for tag in tags_list:
+                t, created = Tag.objects.get_or_create(title=tag)
+                tags_objs.append(t)
+
+            p, created = Post.objects.get_or_create(picture=picture, caption=caption, user_id=user)
+            p.tag.set(tags_objs)
+            p.save()
+            return redirect('index')
+
+    else:
+        form = PostForm()
+        context = {'form': form}
+        return render(request, 'new_post.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    context = {'post': post}
+
+    return render(request, 'post_details.html', context)
