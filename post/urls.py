836abf376302073fe8070e4c0f6bcbeff146f107
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-06-06 11:01:52 +0300

    added a url path to display the liked comment when a user clicks on the like button of a post

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,7 @@ from post import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('new_post/', views.new_post, name='new_post'),
-    path('<uuid:post_id>/', views.post_detail, name='post_detail'),
+    path('<uuid:post_id>/', views.post_detail, name='post-details'),
+    path('tag/<slug:tag_slug>/', views.tags, name='tags'),
+    path('<uuid:post_id>/like', views.like, name='like'),
 ]
